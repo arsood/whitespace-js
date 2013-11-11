85844f36f088c1ac9f5eaa477519aee8359a1883
arsood <arsood@gmail.com>
2013-11-11 14:30:44 -0800

    A few changes

--- js/whitespace.js
+++ js/whitespace.js
@@ -1,4 +1,15 @@
+//Send users to link on well-link click
+
 $(".well-link").click(function() {
 	var location = $(this).attr("data-location");
 	window.location = location;
+});
+
+//Change look of footer on link click
+
+$(".footer-element").click(function() {
+	$(".footer-element").each(function() {
+		$(".footer-element").removeClass("active");
+	});
+	$(this).addClass("active");
 });
